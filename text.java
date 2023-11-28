42a59d54f32036b96daa27892ce4f010168a3e10
Suraj-3579 <152250226+Suraj-3579@users.noreply.github.com>
2023-11-28 23:28:02 +0530

    Create text.java

--- text.java
+++ text.java
@@ -1,6 +1,6 @@
 public class text
   {
   public static void main(String[]a){
-    System.out.println("Hello Everyone , My name is Sushant Shelar");
+    System.out.println("Hello Everyone , My name is Susahnt Shelar");
   }
 }
